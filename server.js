57b23b22f814db43d31f14bd6a032c548b031f95
Abdel Shafy <ahmed.elsherif@igtsservice.com>
2021-07-12 01:08:56 +0200

    First Submission - server and app js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+const bodyParser = require('body-parser');
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +14,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors({origin: 'http://localhost:8000'}));
+
+// Fetch require
+const fetch = require("node-fetch");
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+
+
+// Run server
+const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
+
+//Variables
+const API_KEY = "22d5faca3063061017d034c09875208a";
+const data = [];
+
+
+app.get('/all', (req, res) => {
+  
+  res.send(projectData);
+ 
+});
+
+app.post('/addWeatherData', (req, res) => {
+
+  console.log(req.body);
+  projectData.temp = req.body.temp;
+  projectData.date = req.body.date;
+  projectData.content = req.body.content;
+
+});
+
--- website/app.js
+++ website/app.js
@@ -1,5 +1,108 @@
+
 /* Global Variables */
+const API_KEY = "22d5faca3063061017d034c09875208a";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const zipCodeInput = document.getElementById('zip');
+const feelingInput = document.getElementById('feelings');
+const generateBtn = document.getElementById('generate');
+const zipCodeError = document.getElementById('zip_error');
+let zipCode = '';
+let feelings = '';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+
+
+
+  // Assign Input Field Values 
+generateBtn.addEventListener('click', () => {
+    zipCode = zipCodeInput.value;
+    feelings = feelingInput.value;
+
+    if(zipCode) {
+
+        zipCodeError.innerHTML = "";
+
+        getWeather(zipCode)
+        .then((data) => {
+            //console.log(data);
+
+            if(data.cod === 200){
+
+                postData('/addWeatherData', {temp: data.main.temp, date: newDate, content: feelings});
+
+            }
+           
+        })
+        .then(() => {
+            updateUI();
+        });
+    
+    } else {
+
+        zipCodeError.innerHTML = "Please enter a valid Zip Code";
+
+    }
+    
+});
+
+
+// POST Data Function
+const postData = async ( url = '', data = {})=>{
+    //console.log(url);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+  
+      try {
+        const newData = await response.json();
+        //console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+  
+  // GET Weather Function
+const getWeather = async ( zipCode )=>{
+        const response = await fetch(BASE_URL + zipCode + ',us' + '&appid=' + API_KEY)
+    
+        try {
+          const newData = await response.json();
+          //console.log(newData);
+  
+          if(newData.cod !== 200) {
+              zipCodeError.innerHTML = newData.message;
+          }
+  
+          return newData;
+  
+        }catch(error) {
+            zipCodeError.innerHTML = error.message;
+        }
+    }
+
+
+const updateUI = async () => {
+    const response = await fetch('/all');
+
+    const data = await response.json();
+    document.getElementById('temp').innerHTML = data.temp;
+    document.getElementById('date').innerHTML = data.date;
+    document.getElementById('content').innerHTML = data.content;
+
+    try {
+
+    } catch(error) {
+        console.log('Error', error);
+    }
+}
