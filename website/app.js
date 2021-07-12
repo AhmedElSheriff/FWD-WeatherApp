8e5e012e28b04c09879d5b110a2356d43eef423c
Abdel Shafy <ahmed.elsherif@igtsservice.com>
2021-07-12 18:05:33 +0200

    Fix date

--- website/app.js
+++ website/app.js
@@ -11,7 +11,7 @@ let feelings = '';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
 
 
 
